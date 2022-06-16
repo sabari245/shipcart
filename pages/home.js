84f327454c053b6c91207d431acef7fc9c4d4d4c
sabari245 <sabari2004@hotmail.com>
2022-06-16 19:47:20 +0530

    files arranged

--- pages/home.js
+++ pages/home.js
@@ -1,16 +1,24 @@
-import NavBar from "../components/NavBar";
-import Card from "../components/Card";
-import Footer from "../components/Footer";
-import Banner from "../components/banner";
+import NavBar from "../components/home/NavBar";
+import Card from "../components/home/Card";
+import Footer from "../components/home/Footer";
+import Banner from "../components/home/banner";
 
 function HomeScreen() {
     return ( 
         <div>
         <NavBar />
         <Banner />
-        <div class="flex display-flex flex-wrap justify-center justify-between m-5"><Card /><Card /><Card /><Card /></div>
-        <div class="flex display-flex flex-wrap justify-center justify-between m-5"><Card /><Card /><Card /><Card /></div>
-        <div class="flex display-flex flex-wrap justify-center justify-between m-5"><Card /><Card /><Card /><Card /></div>
+        <div class="flex display-flex flex-wrap justify-center justify-between">
+            {
+                [1,2,3,4,5,6,7,8,9,10].map(item => {
+                    return (
+                        <div className="m-2">
+                            <Card />
+                        </div>
+                    )
+                })
+            }
+        </div>
         <Footer />
         </div>
      );
