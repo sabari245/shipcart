c0fb5d1e452b82df6e9cba8f6670b3181e8eb471
Rahul.B <talktorahul.b@gmail.com>
2022-06-16 21:01:43 +0530

    Product Page Completed

--- components/ProductPages/Trending_Product.js
+++ components/ProductPages/Trending_Product.js
@@ -1,10 +1,8 @@
-import Product1 from "./ProductPages/Product";
-
-function Cards(){
+function Trending_Products(){
     return(
         <div class="bg-white">
   <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-    <h2 class="sr-only">Products</h2>
+    <h2 class="sr-only">Other Products</h2>
 
     <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
       <a href="#" class="group">
@@ -31,7 +29,7 @@ function Cards(){
         <p class="mt-1 text-lg font-medium text-gray-900">$89</p>
       </a>
 
-      <a href={{Product1}} class="group">
+      <a href="#" class="group">
         <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
           <img src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg" alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="w-full h-full object-center object-cover group-hover:opacity-75"/>
         </div>
@@ -46,4 +44,4 @@ function Cards(){
     )
 }
 
-export default Cards;
+export default Trending_Products
--- pages/home.js
+++ pages/home.js
@@ -2,14 +2,15 @@ import NavBar from "../components/home/NavBar";
 import Banner from "../components/home/banner";
 import Footer from "../components/home/Footer";
 import Cards from "../components/Cards";
+import Product1 from "../components/ProductPages/Product";
 
 function HomeScreen() {
     return ( 
         <div>
         <NavBar/>
         <Banner/>
-        <Cards />
-        <Footer />
+       <Cards/>
+       <Footer/>
         </div>
      );
 }
