d13afaa4ee481a388af42f1b371990f662640a4a
Rahul.B <talktorahul.b@gmail.com>
2022-06-16 20:07:48 +0530

    home Page Updated

--- pages/home.js
+++ pages/home.js
@@ -1,24 +1,14 @@
 import NavBar from "../components/home/NavBar";
-import Card from "../components/home/Card";
-import Footer from "../components/home/Footer";
 import Banner from "../components/home/banner";
+import Footer from "../components/home/Footer";
+import Cards from "../components/Cards";
 
 function HomeScreen() {
     return ( 
         <div>
-        <NavBar />
-        <Banner />
-        <div class="flex display-flex flex-wrap justify-center justify-between">
-            {
-                [1,2,3,4,5,6,7,8,9,10].map(item => {
-                    return (
-                        <div className="m-2">
-                            <Card />
-                        </div>
-                    )
-                })
-            }
-        </div>
+        <NavBar/>
+        <Banner/>
+        <Cards />
         <Footer />
         </div>
      );
